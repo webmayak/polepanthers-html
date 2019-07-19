6cdde0758bde92dc1724b5a8f403a03902e5b377
Aileen <aileen.kingsler@mail.ru>
2019-07-19 15:13:13 +1000

    Адаптировала Главную под мобильные

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,21 @@
 $(function () {
 
+  // mainNav
+  var mainNav = document.querySelector('.main-nav');
+  var mainNavToggle = mainNav.querySelector('.main-nav__toggle');
+
+  mainNav.classList.remove('main-nav--no-js');
+  mainNav.classList.remove('main-nav--opened');
+  mainNav.classList.add('main-nav--closed');
+
+  mainNavToggle.addEventListener('click', function() {
+    if (mainNav.classList.contains('main-nav--closed')) {
+      mainNav.classList.remove('main-nav--closed');
+      mainNav.classList.add('main-nav--opened');
+    } else {
+      mainNav.classList.add('main-nav--closed');
+      mainNav.classList.remove('main-nav--opened');
+    }
+  });
+
 });
